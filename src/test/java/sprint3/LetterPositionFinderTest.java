f31c69b2111f5984550e1a17dc1afffb33486394
IrynaKolyada <iryna.kolyada.us@gmail.com>
2024-03-23 08:54:23 -0600

    Letter position test (#33)
    * add LetterPosotionFinderTest
    
    * refactor LetterPositionFinderTest

--- src/test/java/sprint3/LetterPositionFinderTest.java
+++ src/test/java/sprint3/LetterPositionFinderTest.java
@@ -8,14 +8,18 @@ import org.testng.annotations.Test;
 
 public class LetterPositionFinderTest {
 
+     private final String URL = "https://ru.wikipedia.org/wiki/" +
+             "%D0%94%D0%BE%D1%81%D1%82%D0%BE%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%87%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D1%8C";
+
+
+
     @Test
     public void testPositionOfLetterHappyPath(){
         final char letter = 'о';
         final int[] expectedResult = {1, 4, 17};
 
         WebDriver driver = new ChromeDriver();
-        driver.get("https://ru.wikipedia.org/wiki/" +
-                "%D0%94%D0%BE%D1%81%D1%82%D0%BE%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%87%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D1%8C");
+        driver.get(URL);
         String titleOfArticle = driver.findElement(By.cssSelector(".mw-page-title-main")).getText();
 
         int[] actualResult = new LetterPositionFinder().findLetterPositions(titleOfArticle, letter);
@@ -30,8 +34,7 @@ public class LetterPositionFinderTest {
         final int[] expectedResult = {};
 
         WebDriver driver = new ChromeDriver();
-        driver.get("https://ru.wikipedia.org/wiki/" +
-                "%D0%94%D0%BE%D1%81%D1%82%D0%BE%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%87%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D1%8C");
+        driver.get(URL);
         String titleOfArticle = driver.findElement(By.cssSelector(".mw-page-title-main")).getText();
 
         int[] actualResult = new LetterPositionFinder().findLetterPositions(titleOfArticle, letter);

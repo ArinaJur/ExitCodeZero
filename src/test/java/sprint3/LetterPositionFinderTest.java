7d1ff766e7a4cbee2b297e6688a008fcc712c33d
IrynaKolyada <iryna.kolyada.us@gmail.com>
2024-03-23 09:04:57 -0600

    Letter position test (#34)
    * add LetterPosotionFinderTest
    
    * refactor LetterPositionFinderTest
    
    * refactor

--- src/test/java/sprint3/LetterPositionFinderTest.java
+++ src/test/java/sprint3/LetterPositionFinderTest.java
@@ -11,8 +11,6 @@ public class LetterPositionFinderTest {
      private final String URL = "https://ru.wikipedia.org/wiki/" +
              "%D0%94%D0%BE%D1%81%D1%82%D0%BE%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%87%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D1%8C";
 
-
-
     @Test
     public void testPositionOfLetterHappyPath(){
         final char letter = 'о';

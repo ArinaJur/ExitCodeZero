7247d04e2a54cb5321ceaa666e3a3ae09e883bc4
Volhabo <99760694+Volhabo@users.noreply.github.com>
2024-03-23 14:26:38 +0100

    ob/task22 (#30)
    * add:count cyrillic characters
    
    * add:test counter letters
    
    ---------
    
    Co-authored-by: Volha Barouskaya <volhabarouskaya@volhas-macbook-air.home>

--- src/test/java/sprint3/provider/CounterDataProvider.java
+++ src/test/java/sprint3/provider/CounterDataProvider.java
@@ -13,4 +13,26 @@ public class CounterDataProvider {
                 {"", 0}
         };
     }
+
+    @DataProvider(name = "wordDataHappyPath")
+    public Object[][] wordDataHappyPath() {
+
+        return new Object[][]{
+                {"абракадабра", 5},
+                {"АбРакадабра", 7},
+                {"aбpакадабра", 7}, //English letters: word.charAt(0) = 'a', word.charAt(2) = 'p'
+                {"1ая", 2},
+                {"пол-литра", 7},
+                {" ", 0}
+        };
+    }
+
+    @DataProvider(name = "wordDataNegativeScenario")
+    public Object[][] wordDataNegativeScenario() {
+
+        return new Object[][]{
+                {"", 0},
+                {null, -1}
+        };
+    }
 }

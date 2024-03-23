dd89697977a0369fced9fee3072d1cb34fda4e7f
IrynaKolyada <iryna.kolyada.us@gmail.com>
2024-03-23 08:37:37 -0600

    add LetterPosotionFinderTest (#32)

---
+++ src/test/java/sprint3/LetterPositionFinderTest.java
@@ -0,0 +1,42 @@
+package sprint3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LetterPositionFinderTest {
+
+    @Test
+    public void testPositionOfLetterHappyPath(){
+        final char letter = 'о';
+        final int[] expectedResult = {1, 4, 17};
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://ru.wikipedia.org/wiki/" +
+                "%D0%94%D0%BE%D1%81%D1%82%D0%BE%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%87%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D1%8C");
+        String titleOfArticle = driver.findElement(By.cssSelector(".mw-page-title-main")).getText();
+
+        int[] actualResult = new LetterPositionFinder().findLetterPositions(titleOfArticle, letter);
+
+        Assert.assertEquals(actualResult, expectedResult);
+        driver.quit();
+    }
+
+    @Test
+    public void testTextHasNoMatchLetter(){
+        final char letter = '9';
+        final int[] expectedResult = {};
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://ru.wikipedia.org/wiki/" +
+                "%D0%94%D0%BE%D1%81%D1%82%D0%BE%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%87%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D1%8C");
+        String titleOfArticle = driver.findElement(By.cssSelector(".mw-page-title-main")).getText();
+
+        int[] actualResult = new LetterPositionFinder().findLetterPositions(titleOfArticle, letter);
+
+        Assert.assertEquals(actualResult, expectedResult);
+        driver.quit();
+    }
+}

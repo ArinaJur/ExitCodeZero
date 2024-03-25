587002653783a860e359cbb52e23248ccf14b9ea
ViktoriiaKarpus <vikakarpys@gmail.com>
2024-03-25 15:05:26 +0100

    Refactor test

--- src/main/java/sprint3/ReverseWord.java
+++ src/main/java/sprint3/ReverseWord.java
@@ -1,5 +1,16 @@
 package sprint3;
 
+/**
+ * Написать метод, который принимает String параметр и переворачивает текст, например, “stressed” → “desserts”
+ * <p>
+ * Для проверки результата
+ * с помощью алгоритма другого человека преобразовать слово “stressed”, получив expectedResult
+ * зайти на сайт https://onlinestringtools.com/reverse-string
+ * ввести в поле input string слово “stressed””
+ * считать результат из поля reversed string
+ * подтвердить, что программа преобразования текста  работает правильно
+ */
+
 public class ReverseWord {
     public static String getReversedWord(String word) {
         if (word == null || word.isEmpty()) {

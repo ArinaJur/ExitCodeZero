a81eb54048b448653dad7c62ea15200cd117272e
Anna Ek <annaek128@gmail.com>
2024-03-24 21:27:57 -0400

    add RemoveDigitsTest

---
+++ src/test/java/sprint3/RemoveDigitsTest.java
@@ -0,0 +1,49 @@
+package sprint3;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+public class RemoveDigitsTest {
+    private WebDriver driver;
+    private static final String URL = "https://www.textfixer.com/tools/remove-numbers-from-text.php";
+    private static final String TEST_SENTENCE = "неСодержит5цифр";
+
+    @BeforeClass
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        driver = new ChromeDriver(chromeOptions);
+    }
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void testRemoveNumbersFromText() {
+        final String expectedResult = "неСодержитцифр"; // call method .toStringFromLetterNumberToLetter()
+
+        openPage(URL);
+
+        WebElement textarea = driver.findElement(By.xpath("//textarea[@id='newText']"));
+        textarea.sendKeys(TEST_SENTENCE);
+
+        WebElement removeNumbersBtn = driver.findElement(By.name("remove-numbers"));
+        removeNumbersBtn.click();
+
+        String actualResult = textarea.getAttribute("value");
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+    private void openPage(String url) {
+        driver.get(url);
+        driver.manage().window().maximize();
+    }
+}

524dc996ba6a7ba1eb7d7dc5931de82cfe0269c2
Maksim Goncharov <maxg131203@gmail.com>
2024-03-27 16:03:25 -0700

    fix: rename test for the test of task 26 and unused code has been deleted

---
+++ src/test/java/UniqueCharactersMGTest.java
@@ -0,0 +1,41 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@Test
+public class UniqueCharactersMGTest {
+    private static final String URL = "https://ciox.ru/count-the-number-of-unique-characters";
+    private static final String TEST_SENTENCE = "куковали кукушки";
+
+    public void testUniqueCharsCount_Ciox() {
+        final String expectedResult = UniqueCharactersMG.uniqueCharsCount(TEST_SENTENCE);
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get(URL);
+
+        WebElement textarea = driver.findElement(By.xpath("//textarea[@id='text_line']"));
+        textarea.sendKeys(TEST_SENTENCE);
+
+        WebElement button = driver.findElement(By.xpath("//span[@class='button_c inline_c']"));
+        button.click();
+
+        WebElement output = driver.findElement(By.xpath("//input[@class='input_c']"));
+
+        String actualResult = output.getAttribute("value");
+
+        Assert.assertEquals(actualResult, expectedResult);
+
+        driver.quit();
+    }
+
+}

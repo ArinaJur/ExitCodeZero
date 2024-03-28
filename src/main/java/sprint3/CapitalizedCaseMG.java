1a0abb1933ea0151673feb8e3422b760d11d0407
Maksim Goncharov <maxg131203@gmail.com>
2024-03-27 18:07:37 -0700

    add tasks and tests

---
+++ src/main/java/sprint3/CapitalizedCaseMG.java
@@ -0,0 +1,33 @@
+package sprint3;
+
+public class CapitalizedCaseMG {
+    public String capitalize(String sentence) {
+
+        //проверка на неправильный аргумент
+        if (sentence == null) {
+            return "Server Error. Please try again.";
+        }
+
+        if (sentence.isEmpty()) {
+            return "Empty input.";
+        }
+
+        StringBuilder result = new StringBuilder();
+        String[] words = sentence.split("\\s+"); //создание массива слов с разделением по пробелу
+
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+
+            //если полученная строка не пустая, преобразуем первый символ в верхний регистр
+            if (!word.isEmpty()) {
+                String capitalizedWord = word.substring(0, 1).toUpperCase() + word.substring(1);
+                result.append(capitalizedWord);
+            }
+            //добавляем преобразовнное слово в новую строку и ставим пробел после каждого, кроме последнего
+            if (i < words.length - 1) {
+                result.append(" ");
+            }
+        }
+        return result.toString();
+    }
+}
